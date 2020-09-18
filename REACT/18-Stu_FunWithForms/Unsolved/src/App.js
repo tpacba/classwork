c38876edaa3c4f8477eb8b3d1d4b74219059bc44
Tristan Pacba <tpacba@live.com>
2020-09-18 13:54:04 -0700

    sep18

--- REACT/18-Stu_FunWithForms/Unsolved/src/App.js
+++ REACT/18-Stu_FunWithForms/Unsolved/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import Counter from "./components/Counter";
+import Form from "./components/Form";
 
 function App() {
-  return <Counter />;
+  return <Form />;
 }
 
 export default App;
