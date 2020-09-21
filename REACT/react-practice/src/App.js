9623315d08941af8a272e22adb96919b9db84041
Tristan Pacba <tpacba@live.com>
2020-09-21 11:17:38 -0700

    sep21

--- REACT/react-practice/src/App.js
+++ REACT/react-practice/src/App.js
@@ -1,8 +1,39 @@
-import React from "react";
-import Counter from "./components/Counter";
+import React from 'react';
+import FriendCard from './components/FriendCard';
+import Wrapper from './components/Wrapper';
+import Title from './components/Title';
+import friends from './friends.json';
 
-function App() {
-  return <Counter />;
+
+class App extends React.Component {
+  state = {
+    friendArray: friends
+  };
+
+  deleteFriend = (id) => {
+    const result = this.state.friendArray.filter(item => item.id !== id)
+
+    this.setState({friendArray: result})
+  }
+
+  render() {
+    return (
+      <Wrapper>
+        <Title>Friends List</Title>
+          {this.state.friendArray.map(item => (
+            <FriendCard
+            key={item.id}
+            id={item.id}
+            name={item.name}
+            image={item.image}
+            occupation={item.occupation}
+            location={item.location}
+            delete={this.deleteFriend}
+            />
+          ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
